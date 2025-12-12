7375d4c75653cb3cf51976a869bc4b097ca96997
DominicDev <s0l0estudi4r@gmail.com>
2025-12-12 11:12:24 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LISA';
+export let nameSongs = [
+	"GURENGE ", /*1*/
+	"CROSSING FIELD", /*2*/
+	"GURENGE (INSTRUMENTAL) ", /*3*/
+	"RISING HOPE", /*4*/
+	"OATH SIGN", /*5*/
+	"SHIRUSHI", /*6*/
+	"CATCH THE MOMENT", /*7*/
+	"明け星", /*8*/
+	"白銀", /*9*/
+	"往け", /*10*/
+	"ADAMAS", /*11*/
+	"UNLASTING", /*12*/
+	"THRILL, RISK, HEARTLESS", /*13*/
+	"DAWN", /*14*/
+	"LITTLE DEVIL PARADE", /*15*/
+	"HOMURA", /*16*/
+	"AKEBOSHI", /*17*/
+	"AIMER", /*18*/
+	"KIZUNA NO KISEKI", /*19*/
+	"AKAI WANA (WHO LOVES IT?)", /*20*/
+]
